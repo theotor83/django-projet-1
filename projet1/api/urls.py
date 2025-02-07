7f498de51c69c72036794c6e410e3c1ee62ec605
Théo Torregrossa <theotor83@gmail.com>
2025-02-07 16:59:52 +0100

    Merged /api/getAllEntries/ inside /api/entries/<id>
    TODO: Add more methods to /api/entries/<id>

--- projet1/api/urls.py
+++ projet1/api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.testGet),
-    path('getAllEntries/', views.getAllEntries),
     path('token/', views.manage_token, name='manage-token'),
     path('testAuth/', views.test_auth, name='test-auth'),
-    path('entries/<int:userid>/', views.entries, name='entries'),
+    path('entries/', views.entries, name='entries'),
+    path('entries/<int:userid>/', views.entries, name='entries-details'),
 ]
--- projet1/api/views.py
+++ projet1/api/views.py
@@ -18,16 +18,6 @@ def testGet(request):
     test = {"test_key":1, "test_key_2":2}
     return Response(test)
 
-@api_view(['GET'])
-@authentication_classes([CustomTokenAuthentication])
-def getAllEntries(request):
-    if request.user.id == 1: #admin
-        allEntries = TodoEntry.objects.all()
-        serializer = EntrySerializer(allEntries, many=True)
-        return JsonResponse({"entries" : serializer.data}, status=200)
-    else:
-        return Response({'error': 'This is an admin only endpoint'}, status=403)
-
 
 @api_view(['POST', 'PUT', 'DELETE'])
 @permission_classes([AllowAny])
@@ -63,24 +53,33 @@ def test_auth(request):
 @api_view(['GET', 'POST'])
 @authentication_classes([CustomTokenAuthentication])
 @permission_classes([IsAuthenticated])
-def entries(request, userid):
-    if request.method == 'GET':
-        if request.user.id == userid or request.user.id == 1 : #1 = admin
-            userid = get_object_or_404(User, pk=userid)
-            entries = TodoEntry.objects.filter(user=userid)
-            serializer = EntrySerializer(entries, many=True)
-            return JsonResponse({"entries" : serializer.data}, status=200)
-        else:
-            return JsonResponse({'error': '403 Unauthorized'}, status=403)
-    if request.method == 'POST':
-        if request.user.id == userid or request.user.id == 1 : #1 = admin
-            userid = get_object_or_404(User, pk=userid)
-            serializer = EntrySerializer(data=request.data, context={'request': request})
-            if serializer.is_valid():
-                # Associate the entry with the user from the URL
-                serializer.save(user=userid)
-                return JsonResponse(serializer.data, status=201)
-            # Return errors if data is invalid.
-            return JsonResponse(serializer.errors, status=400)
-        else:
-            return JsonResponse({'error': '403 Forbidden'}, status=403)
+def entries(request, requestedUserId = None):
+
+    if requestedUserId == None: #if /api/entries/ instead of /api/entries/<id>
+        if request.method == 'GET':
+            if request.user.id == 1: #admin
+                allEntries = TodoEntry.objects.all()
+                serializer = EntrySerializer(allEntries, many=True)
+                return JsonResponse({"entries" : serializer.data}, status=200)
+            else:
+                return Response({'error': '403 Unauthorized'}, status=403)
+            
+    else:
+        if request.method == 'GET':
+            if request.user.id == requestedUserId or request.user.id == 1 : #1 = admin
+                requestedUserId = get_object_or_404(User, pk=requestedUserId)
+                entries = TodoEntry.objects.filter(user=requestedUserId)
+                serializer = EntrySerializer(entries, many=True)
+                return JsonResponse({"entries" : serializer.data}, status=200)
+            else:
+                return JsonResponse({'error': '403 Unauthorized'}, status=403)
+        if request.method == 'POST':
+            if request.user.id == requestedUserId or request.user.id == 1 : #1 = admin
+                requestedUserId = get_object_or_404(User, pk=requestedUserId)
+                serializer = EntrySerializer(data=request.data, context={'request': request})
+                if serializer.is_valid():
+                    serializer.save(user=requestedUserId)
+                    return JsonResponse(serializer.data, status=201)
+                return JsonResponse(serializer.errors, status=400)
+            else:
+                return JsonResponse({'error': '403 Unauthorized'}, status=403)
